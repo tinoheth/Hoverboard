d00806af6bfcfec0e423e3212d424684ffb99f76
Tino Heth <th@t-no.de>
2016-12-14 19:18:02 +0100

    Initial Commit

---
+++ Hoverboard/Hoverboard.h
@@ -0,0 +1,19 @@
+//
+//  Hoverboard.h
+//  Hoverboard
+//
+//  Created by Tino Heth on 14.12.16.
+//  Copyright © 2016 Tino Heth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Hoverboard.
+FOUNDATION_EXPORT double HoverboardVersionNumber;
+
+//! Project version string for Hoverboard.
+FOUNDATION_EXPORT const unsigned char HoverboardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hoverboard/PublicHeader.h>
+
+
